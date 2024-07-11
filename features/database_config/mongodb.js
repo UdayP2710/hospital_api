8ae62ca588472a2274b27ff65e842fc36304519b
UdayP2710 <153024688+UdayP2710@users.noreply.github.com>
2024-07-11 15:01:37 +0530

    Update mongodb.js

--- features/database_config/mongodb.js
+++ features/database_config/mongodb.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 export const connectToDatabase = async () => {
   try {
     console.log("Connecting to database.....");
-    await mongoose.connect("mongodb://49.36.221.187:27017/hospital_API");
+    await mongoose.connect("mongodb://localhost:27017/hospital_API");
     console.log("Database connected successfully!!!!");
   } catch (err) {
     console.log("something went wrong while connecting to database!!!!!"+"  "+ err);
