b1f0200464f763396dabfbcc818b918f3e423597
UdayP2710 <153024688+UdayP2710@users.noreply.github.com>
2024-07-15 14:22:40 +0530

    updated db connection string

--- features/database_config/mongodb.js
+++ features/database_config/mongodb.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 export const connectToDatabase = async () => {
   try {
     console.log("Connecting to database.....");
-    await mongoose.connect("mongodb://localhost:27017/hospital_API");
+    await mongoose.connect("mongodb+srv://udaypandey20000:nVE8XlR2OxLPeEOE@cluster0.kyg0z1c.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
     console.log("Database connected successfully!!!!");
   } catch (err) {
     console.log("something went wrong while connecting to database!!!!!"+"  "+ err);
